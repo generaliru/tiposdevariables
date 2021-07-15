2a464fa1dd23ee1d7d9ac54a0c4badc226d1a824
generaliru <ucamcahoa1600@alumno.ipn.mx>
2021-07-15 18:00:23 -0500

    Clase tipos de variables

---
+++ src/TipoDeDatos.java
@@ -0,0 +1,16 @@
+
+public class TipoDeDatos {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hola Mundo");
+		
+		int numero1 = 1;
+		int numero2 = 3;
+		int multiplicacion = numero1 * numero2;
+		boolean mayor18 = true;
+		
+		System.out.println(multiplicacion);
+	}
+
+}
